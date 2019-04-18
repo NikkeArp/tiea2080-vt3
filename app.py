b0be40fba268b70420a0f9448c4a0ffabd713faf
Niklas Seppala <nikke40@gmail.com>
2019-04-18 22:24:03 +0300

    modules and base for grater things

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 #!venv/bin/python
 # -*- coding: utf-8 -*-
 
+#Basic modules
+import simplejson as json
 import hashlib
-import logging
-from flask import Flask, render_template, url_for
+import os
 
-#---------DEBUG SETTINGS----------#
-logging.basicConfig(filename='logs/server.log', level=logging.DEBUG)
-#---------------------------------#
+#My modules
+from mylogging import log_exc, logging
+
+#Flask modules
+from flask_wtf import FlaskForm
+from wtforms import StringField, Form, validators, SubmitField
+from wtforms.validators import DataRequired
+from flask import Flask, render_template, url_for, redirect, request, session
+
 
 #--------App configurations-------#
 app = Flask(__name__)
@@ -15,12 +22,34 @@ app.secret_key='dbd238d94e15178b1e6440b09749d1e66847152487e932df0ffc0536b78ff2a5
 #---------------------------------#
 
 
-@app.route('/')
+#----------Load json--------------#
+try:
+    with open("data/data.json") as file:
+        data = json.load(file)
+except:
+    log_exc()
+    logging.warning("DATA-JSON DIDN'T LOAD!")
+    data = None
+#---------------------------------#
+
+###################################
+##          WTF-FORMS            ##
+###################################
+
+
+###################################
+##            ROUTES             ##
+###################################
+
+@app.route('/', methods=("GET", "POST"))
 def index():
-    logging.info("Rendered index template")
     return render_template("index.html")
+    
 
-
+###################################
+##           RUN APP             ##
+###################################
 
 if __name__ == '__main__':
+    logging.info("App is running! http://127.0.0.1:5000/\n")
     app.run(debug=True)
