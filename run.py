f91cf72913224710401d0dc073e72279d328acbc
Niklas Seppala <nitasepp@student.jyu.fi>
2019-05-08 14:47:24 +0300

    base.html and configs

--- run.py
+++ run.py
@@ -1,8 +1,17 @@
 #!venv/bin/python
 #-*- coding: utf-8 -*-
 from vt3 import create_app
+from flask import session, render_template
+from flask.logging import create_logger
 
 app = create_app()
+logger = create_logger(app)
+
+@app.route('/')
+def hello():
+   session['testi'] = 'asd :)'
+   logger.debug('asddddddddd')
+   return render_template('index.html')
 
 if __name__ == '__main__':
    app.run()
--- vt3/__init__.py
+++ vt3/__init__.py
@@ -2,17 +2,26 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask
+from flask_debugtoolbar import DebugToolbarExtension
+
+toolbar = DebugToolbarExtension()
+logger = None
 
 def create_app(config=None):
     app = Flask(__name__)
     app.config.from_mapping({
             'SECRET_KEY': '\xb8o\x9f\x91\x07\x93{Z\x947\xc2s(\x9f',
             'LOGPATH': 'logs/server.log',
+            'ENV': 'developement',
             'JSONPATH': 'data/data.json',
             'DEBUG': True
         })
-
     if config:
         app.config.from_mapping(config)
+    
+    app.jinja_env.trim_blocks = True
+    app.jinja_env.lstrip_blocks = True
+
+    toolbar.init_app(app)    
 
     return app
