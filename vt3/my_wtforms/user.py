3f48ad34e8e745b458f5de22ec926fec7217ae2e
Niklas Seppala <nikke40@gmail.com>
2019-05-11 10:13:57 +0300

    taso 1

--- vt3/my_wtforms/user.py
+++ vt3/my_wtforms/user.py
@@ -8,19 +8,44 @@ from hashlib import sha512
 from vt3.mylogging import log_exception
 
 ## Flask modules
-from flask import g, current_app
+from flask import g, current_app, session
 from flask_wtf import FlaskForm
-from wtforms.fields import StringField, SelectField, PasswordField
+from wtforms.fields import (StringField, SelectField, PasswordField,
+    HiddenField, RadioField)
 from wtforms import validators
 
 logger = current_app.logger
+
+def validate_teamname(form):
+    '''Helper function to make sure user
+    input teamname is unique'''
+
+    ## No changes to teamname
+    if session['user']['name'].upper() == form.name.data.upper():
+        return True
+
+    ## Find possible match from other teams
+    for race in g.data:
+        if race['nimi'] == session['race']:
+            for series in race['sarjat']:
+                for team in series['joukkueet']:
+                    if team['nimi'].upper() == form.name.data.upper():
+                        form.name.errors.append(u'Kilpailussa on jo joukkue tällä nimellä.')
+                        return False
+    ## Name is unique
+    return True
 
 def validate_team(data, form):
     for race in data:
         if race['nimi'] == form.race.data:
             for serie in race['sarjat']:
                 for team in serie['joukkueet']:
-                    if team['nimi'] == form.username.data:
+                    
+                    ## Ignore whitespace and capitalization
+                    if team['nimi'].upper().strip() == form.username.data.upper():
+                        g.team_name = team['nimi']
+                        g.team = team
+                        g.series = serie['nimi']
                         return True
     def any_race():
         for race in data:
@@ -37,6 +62,9 @@ def validate_team(data, form):
     return False
 
 def validate_password(form, field):
+    '''Validates users input password by hashing it and 
+    comparing to predefined password hash.'''
+    
     hasher = sha512(current_app.config['SECRET_KEY'] +
         str(field.data))
     if hasher.hexdigest() == current_app.config['PASS_W']:
@@ -52,21 +80,25 @@ class LoginForm(FlaskForm):
     are set dynamically after form initialization.'''
 
     username = StringField(u'Joukkue',
-        validators=[validators.DataRequired()])
+        validators=[validators.DataRequired()],
+        #? Strip whitespace and raise to upper for validation
+        filters=[lambda x: x.strip() if x else x])
     password = PasswordField(u'Salasana',
         validators=[validators.DataRequired()])
     race = SelectField(u'Kilpailu',
         validators=[validators.DataRequired()])
     
     def set_choices(self):
+        '''Enables dynamic choices after forms creation'''
         races = []
         for i, race in enumerate(g.data):
             races.append((race['nimi'], race['nimi']))
         self.race.choices=races
 
     def validate(self):
+        '''Basic validation with custom validators for
+        username and password'''
         if FlaskForm.validate(self):
-
             if validate_team(g.data, self):
                 if validate_password(self, self.password):
                     return True
@@ -75,4 +107,75 @@ class LoginForm(FlaskForm):
         return False
         
         
+##                                                                ##
+##                          EditUserForm                          ##
+##                                                                ##
+
+
+class EditTeamForm(FlaskForm):
+    name = StringField(u'Nimi',
+        validators=[validators.InputRequired()])
+
+    series = RadioField(u'Sarja',
+        validators=[validators.InputRequired()])
+
+    mem1 = StringField(u'Jäsen 1')
+    mem2 = StringField(u'Jäsen 2')
+    mem3 = StringField(u'Jäsen 3')
+    mem4 = StringField(u'Jäsen 4')
+    mem5 = StringField(u'Jäsen 5')
+
+    def validate(self):
+        '''Basic validation with custom validators for team-members and
+        teamname. Ensures team name is unique and team has min 2 members.'''
+        if FlaskForm.validate(self):
+
+            validation_results = []
+
+            ##Validate team members, min 2
+            members = [
+                self.mem1.data,self.mem2.data,
+                self.mem3.data,self.mem4.data,
+                self.mem5.data
+            ]
+            if len(filter(lambda x: x != u'' and x != None, members)) >= 2:
+                validation_results.append(True)
+            else:
+                self.mem1.errors.append(u'Vähintään 2 jäsentä')
+                validation_results.append(False)
+
+            ##Validate team name, unique name
+            validation_results.append(validate_teamname(self))
+
+            return all(validation_results)
+        else:
+            return False
+
+
+    def init_series(self, race_name):
+        '''Sets values to radiofield selections'''
+        series = None
+        for race in g.data:
+            if race['nimi'] == race_name:
+                series = race['sarjat']
+        if series:
+            series = map(lambda x: (x['nimi'], x['nimi']), series)
+            self.series.choices = sorted(series)
+            
+
+    def set_defaults(self):
+        '''Sets defailt values to form-fields'''
+        self.name.default = session['user']['name']
+        self.series.default = session['user']['series']
+        
+        try:
+            self.mem1.default = session['user']['members'][0]
+            self.mem2.default = session['user']['members'][1]
+            self.mem3.default = session['user']['members'][2]
+            self.mem4.default = session['user']['members'][3]
+            self.mem5.default = session['user']['members'][4]
+        except:
+            pass
+
+        self.process()
 
--- vt3/user.py
+++ vt3/user.py
@@ -3,7 +3,7 @@
 
 ## My python modules
 from mylogging import log_exception
-from my_wtforms.user import LoginForm
+from my_wtforms.user import LoginForm, EditTeamForm
 
 ## Basic python modules
 from functools import wraps
@@ -13,9 +13,6 @@ from flask import Blueprint, flash, redirect, render_template, url_for
 from flask.globals import current_app, g, request, session
 import flask.json as json
 
-
-##            Define user blueprint             ##
-
 user = Blueprint('user', __name__)
 log = current_app.logger
 
@@ -23,25 +20,33 @@ log_messages = []
 def log_in_view(message, logger=log):
     log_messages.append((message, logger))
 
-
 ##        Load race-data from data.json         ##
-
 
 @user.before_request
 def before_request():
     try:
         with open(current_app.config['JSONPATH'], 'r') as file:
             g.data = json.load(file)
-        log_in_view('JSON-data successfuly loaded')
     except:
         g.data = None
         log_in_view(log_exception(log))
         log_in_view('JSON-data didnt load!')
+        
 
+def save_json():
+    try:
+        with open(current_app.config['JSONPATH'], 'w') as file:
+            json.dump(g.data, file)
+    except:
+        log_in_view(log_exception(log))
+        log_in_view('JSON didnt get saved!')
 
 ##    Decorator-functions for view-functions    ##
 
 def log_debug(func):
+    '''Decorator function to log messages in view-function.
+       This way, debug-toolbar can show logged messages outside
+       of view-functions.'''
     @wraps(func)
     def decorated(*args, **kwargs):
         for x in log_messages:
@@ -49,6 +54,9 @@ def log_debug(func):
         return func(*args, **kwargs)
     return decorated
 def authorized(func):
+    '''Decorator function to make sure user is logged in
+       before accessing content-pages. If not authorized,
+       redirects user to login-page.'''
     @wraps(func)
     def decorated(*args, **kwargs):
         if not session.get('logged_in'):
@@ -56,6 +64,9 @@ def authorized(func):
         return func(*args, **kwargs)
     return decorated
 def json_loaded(func):
+    '''Decorator-function to ensure that JSON-data is
+       loaded before executing view-function.
+       Redirects user to JSON-error page.'''
     @wraps(func)
     def decorated(*args, **kwargs):
         if not getattr(g, 'data', None):
@@ -68,23 +79,119 @@ def json_loaded(func):
 
 @user.route('/json-error')
 def json_error():
+    '''JSON-error page to inform user about JSON-error.'''
     return render_template('JSON_e.html')
 
 @user.route('/', methods=['GET', 'POST'])
 @user.route('/login', methods=['GET', 'POST'])
-@log_debug
+##@log_debug
 @json_loaded
 def login():
     '''Login view-function'''
 
+    ## Initialize login-form and set choices dynamically.
     form = LoginForm()
     form.set_choices()
 
     if form.validate_on_submit():
-        session['user'] = form.username.data
-        log.debug(u'User {0} logged in'.format(session['user']))
-        return u'Hello {0}!'.format(session['user'])
+        session['user'] = {
+            'name': g.team['nimi'], #? Actual teamname from validation (not user input).
+            'series': g.series, #? Series from validation
+            'members': sorted(g.team['jasenet']), #? Team members from validation
+            'id': g.team['id']
+        } 
+        session['race'] = form.race.data  #? Race name from form.
+        session['logged_in'] = True
 
+        ## Redirect user to their races teams-page
+        return redirect(url_for('user.teams'))
+
+    ## Render login-page
     return render_template('user/login.html',
         form=form)
 
+@user.route('/logout')
+@authorized
+##@log_debug
+def logout():
+    '''Logout view-function. Clears session variables but
+       sets logged_in to False. Redirects user to login-page.'''
+    session.clear()
+    session['logged_in'] = False
+    return redirect(url_for('user.login'))
+
+
+@user.route('/teams')
+@authorized
+@log_debug
+@json_loaded
+##@log_debug
+def teams():
+    '''Teams view-function for selected race.
+       Displays all teams and members.'''
+
+    for r in g.data:
+        race = r if r['nimi'] == session['race'] else None
+        if race: break
+    
+    race['sarjat'] = sorted(race['sarjat'], key=lambda x: x['nimi'])
+    return render_template('user/teams.html',
+        race=race)
+
+
+@user.route('/team', methods=['GET', 'POST'])
+@authorized
+@json_loaded
+def team():
+
+    form = EditTeamForm()
+    form.init_series(session['race'])
+
+    if form.validate_on_submit():
+
+        team = None
+        series_modified = True if session['user']['series'] != form.series.data else False    
+
+        for race in g.data:
+            if race['nimi'] == session['race']:
+                for series in race['sarjat']:
+                    if series['nimi'] == session['user']['series']:
+                        for i, t in enumerate(series['joukkueet']):
+                            if t['id'] == session['user']['id']:
+                                team = t
+                                if series_modified:
+                                    del series['joukkueet'][i]
+                                else:
+                                    t['nimi'] = form.name.data
+                                    t['jasenet'] = filter(lambda x: x != u'', [
+                                        form.mem1.data, form.mem2.data, form.mem3.data,
+                                        form.mem4.data, form.mem5.data])
+        
+        session['user']['name'] = form.name.data
+        session['user']['members'] = filter(lambda x: x != u'', [
+            form.mem1.data, form.mem2.data, form.mem3.data,
+            form.mem4.data, form.mem5.data])
+        
+        if series_modified:
+            session['user']['series'] = form.series.data
+            
+            team['nimi'] = form.name.data
+            team['jasenet'] = filter(lambda x: x != u'', [
+                form.mem1.data, form.mem2.data, form.mem3.data,
+                form.mem4.data, form.mem5.data])
+
+            for race in g.data:
+                if race['nimi'] == session['race']:
+                    for series in race['sarjat']:
+                        if series['nimi'] == session['user']['series']:
+                            series['joukkueet'].append(team)
+                            log.debug(team)
+
+        session.modified = True
+        save_json()
+        return redirect(url_for('user.team'))
+
+    if not form.is_submitted():
+        form.set_defaults()
+
+    return render_template('user/team.html', form=form)
